7bb5d736c86027b641d7c6766098a6dc044d04f3
Michael Bláha <blahami2@gmail.com>
2016-10-14 16:40:13 +0200

    Optimizations for dataset

--- src/main/java/cz/blahami2/routingsaratester/generator/logic/DataSetGenerator.java
+++ src/main/java/cz/blahami2/routingsaratester/generator/logic/DataSetGenerator.java
@@ -14,14 +14,18 @@ import cz.certicon.routing.model.graph.Edge;
 import cz.certicon.routing.model.graph.Graph;
 import cz.certicon.routing.model.graph.Metric;
 import cz.certicon.routing.model.graph.Node;
+import cz.certicon.routing.model.values.Coordinate;
 import cz.certicon.routing.model.values.Distance;
 import cz.certicon.routing.model.values.Time;
 import cz.certicon.routing.model.values.TimeUnits;
+import cz.certicon.routing.utils.RandomUtils;
 import cz.certicon.routing.utils.java8.Optional;
 import cz.certicon.routing.utils.measuring.TimeMeasurement;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -41,9 +45,13 @@ public class DataSetGenerator {
      * @param <N> node type of the graph
      * @param <E> edge type of the graph
      * @param size amount of data elements, not guaranteed - see attemptsPerUnit
-     * @param granularity amount of "buckets", higher the amount, more uniform the distribution
-     * @param attemptsPerUnit maximal amount of attempts per unit (size) for the algorithm to fill all the buckets, then it terminates
-     * @param estimatedMaximalDistance estimated distance of maximal route, no inputs will be higher than this value, however, fewer inputs will be found if it is too high
+     * @param granularity amount of "buckets", higher the amount, more uniform
+     * the distribution
+     * @param attemptsPerUnit maximal amount of attempts per unit (size) for the
+     * algorithm to fill all the buckets, then it terminates
+     * @param estimatedMaximalDistance estimated distance of maximal route, no
+     * inputs will be higher than this value, however, fewer inputs will be
+     * found if it is too high
      * @param metric routing metric
      * @param graph graph to search on
      * @return dataset
@@ -60,6 +68,7 @@ public class DataSetGenerator {
         for ( int i = 0; i < intervals; i++ ) {
             intervalList.add( new ArrayList<>() );
         }
+        /*
         Set<Integer> unfinished = IntStream.range( 0, intervals ).boxed().collect( Collectors.toSet() );
         Iterator<Pair<N, N>> generator = nodePairGenerator.generatorIterator( graph );
         int sum = 0;
@@ -100,13 +109,75 @@ public class DataSetGenerator {
                     }
                 }
             }
+        }*/
+
+        int numberOfNodes = 1000;
+        NodeGenerator nodeGenerator = new NodeGenerator();
+        Random rand = RandomUtils.createRandom();
+        for ( int i = 0; i < inputsPerInterval; i++ ) {
+            Set<N> nodeSet = new HashSet<>();
+            Iterator<N> generatorIterator = nodeGenerator.generatorIterator( graph );
+            while ( nodeSet.size() < numberOfNodes ) {
+                nodeSet.add( generatorIterator.next() );
+            }
+            List<N> nodes = new ArrayList<>( nodeSet );
+            List<Pair<N, N>> pairs = nodes.stream()
+                    .flatMap( n1
+                            -> nodes.stream().map( n2 -> new Pair<>( n1, n2 ) )
+                    ).sorted( ( p1, p2 ) -> compare( p1, p2 ) )
+                    .collect( Collectors.toList() );
+            Set<Integer> unfinished1 = IntStream.range( 0, intervals ).boxed().collect( Collectors.toSet() );
+            for ( int j = 0; j < intervals; j++ ) {
+                if ( unfinished1.contains( j ) ) {
+                    int direction = 0;
+                    for ( int k = 0; k < maxIterations && unfinished1.contains( j ); k++ ) {
+                        int intervalIdx = j;
+                        int minLength = intervalSize * j;
+                        int maxLength = intervalSize * ( j + 1 );
+                        int nodeIntervalSize = ( numberOfNodes * numberOfNodes ) / intervals;
+                        int idx = nodeIntervalSize * ( j + direction ) + rand.nextInt( nodeIntervalSize );
+                        Pair<N, N> pair = pairs.get( idx );
+                        Optional<Route<N, E>> optionalRoute = algorithm.route( graph, metric, pair.a, pair.b );
+                        if ( optionalRoute.isPresent() ) {
+                            Route<N, E> route = optionalRoute.get();
+                            Distance distance = route.calculateDistance( metric );
+                            int length = (int) distance.getValue();
+                            if ( length < minLength ) {
+                                direction++;
+                            } else if ( length > maxLength ) {
+                                direction--;
+                            } else {
+                                List<DataSetElement<N, E>> targetInterval = intervalList.get( intervalIdx );
+                                targetInterval.add( new DataSetElement<>( pair.a, pair.b, route ) );
+                                unfinished1.remove( j );
+                                System.out.println( "added to bucket[" + intervalIdx + "] => " + targetInterval.size() );
+                            }
+                        }
+                        direction = direction < 0 ? 0 : direction;
+                        direction = direction > intervals - j - 1 ? intervals - j - 1 : direction;
+                    }
+                }
+            }
         }
+
         return intervalList.stream()
                 .flatMap(
                         x -> x.stream().sorted(
                                 ( a, b ) -> a.getDistance( metric ).compareTo( b.getDistance( metric ) )
                         )
                 ).collect( Collectors.toList() );
+    }
+
+    private static <N extends Node<N, E>, E extends Edge<N, E>> int compare( Pair<N, N> pair1, Pair<N, N> pair2 ) {
+        double dist1 = calculateDistance( pair1.a.getCoordinate(), pair1.b.getCoordinate() );
+        double dist2 = calculateDistance( pair2.a.getCoordinate(), pair2.b.getCoordinate() );
+        return Double.compare( dist1, dist2 );
+    }
+
+    private static double calculateDistance( Coordinate c1, Coordinate c2 ) {
+        double diffLat = c1.getLatitude() - c2.getLatitude();
+        double diffLon = c1.getLongitude() - c2.getLongitude();
+        return diffLat * diffLat + diffLon * diffLon;
     }
 
 }
---
+++ src/main/java/cz/blahami2/routingsaratester/generator/logic/NodeGenerator.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.blahami2.routingsaratester.generator.logic;
+
+import cz.blahami2.routingsaratester.utils.Iterators;
+import cz.certicon.routing.model.basic.Pair;
+import cz.certicon.routing.model.graph.Edge;
+import cz.certicon.routing.model.graph.Graph;
+import cz.certicon.routing.model.graph.Node;
+import cz.certicon.routing.utils.RandomUtils;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ *
+ * @author Michael Blaha {@literal <michael.blaha@gmail.com>}
+ */
+public class NodeGenerator {
+
+    public <N extends Node<N, E>, E extends Edge<N, E>> Stream<Pair<N, N>> generatorStream( Graph<? extends N, ? extends E> graph ) {
+        return StreamSupport.stream( new NodeSpliterator( graph ), false );
+    }
+
+    public <N extends Node<N, E>, E extends Edge<N, E>> Iterator<N> generatorIterator( Graph<? extends N, ? extends E> graph ) {
+        return new NodeIterator( graph );
+    }
+
+    private static class NodeIterator<N extends Node<N, E>, E extends Edge<N, E>> implements Iterator<N> {
+
+        private final Random rand = RandomUtils.createRandom();
+        private final Node[] nodes;
+
+        public NodeIterator( Graph<N, E> graph ) {
+            this.nodes = Iterators.stream( graph.getNodes() ).collect( Collectors.toList() ).toArray( new Node[]{} );
+        }
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public N next() {
+            return (N) nodes[rand.nextInt( nodes.length )];
+        }
+
+    }
+
+    private static class NodeSpliterator<N extends Node<N, E>, E extends Edge<N, E>> implements Spliterator<N> {
+
+        private final Random rand = RandomUtils.createRandom();
+        private final Node[] nodes;
+
+        public NodeSpliterator( Graph<? extends N, ? extends E> graph ) {
+            this.nodes = Iterators.stream( graph.getNodes() ).collect( Collectors.toList() ).toArray( new Node[]{} );
+        }
+
+        @Override
+        public boolean tryAdvance( Consumer<? super N> action ) {
+            Node n = nodes[rand.nextInt( nodes.length )];
+            action.accept( (N) n );
+            return true;
+        }
+
+        @Override
+        public Spliterator<N> trySplit() {
+            return null;
+        }
+
+        @Override
+        public long estimateSize() {
+            return Long.MAX_VALUE;
+        }
+
+        @Override
+        public int characteristics() {
+            return 0;
+        }
+
+    }
+}
